65272441c3b8ea00e930e2939b04853b761e7760
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-09-13 19:16:42 -0700

    Moved all of the hard coded HTML out of the App.js file
    Moved all of the hardcoded divs out of the file. Deleted the logo and the service worker since it won't be used in this project. Will be building this app component by component so need to make sure to keep things straight

---
+++ my-reads/src/components/MenuBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//The menubar is a stateless component so we can define it just in a function.
+function MenuBar(props) {
+  return(
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      </div>
+  )
+}
+
+export default MenuBar;
