8e450bc9135cf37280ea690bad833c6c07226ceb
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-09-13 20:05:05 -0700

    Set up the Book API to get all books and then sort those books.

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import * as BooksAPI from './BooksAPI'
+import './App.css';
 import MenuBar from './components/MenuBar';
-import BookShelf from './components/BookShelf'
+import BookShelf from './components/BookShelf';
 
 class BooksApp extends React.Component {
   state = {
@@ -12,16 +12,43 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+     allBooks: [],
+     currentlyReading: [],
+     wantToRead: [],
+     read: [],
     showSearchPage: false
   }
+componentDidMount()  {
+  BooksAPI.getAll().then((books)=> {
+  this.setState({allBooks: books})
+
+}).then(() => {
+  this.sortBooks(this.state.allBooks);
+})
+}
+
+sortBooks = (books) => {
+  this.setState(() => ({
+    currentlyReading: books.filter((book) => {
+      return book.shelf === "currentlyReading"
+    }),
+    wantToRead: books.filter((book) => {
+      return book.shelf === "wantToRead"
+    }),
+    read: books.filter((book) => {
+      return book.shelf === "read"
+    })
+  }))
+}
+
 
   render() {
     return (
       <div className="app">
       <MenuBar/>
-      <BookShelf bookStatus="Currently Reading"/>
-      <BookShelf bookStatus="Want to Read"/>
-      <BookShelf bookStatus="Finished Reading"/>
+      <BookShelf bookStatus="Currently Reading" booksList={this.state.currentlyReading}/>
+      <BookShelf bookStatus="Want to Read" booksList={this.state.wantToRead}/>
+      <BookShelf bookStatus="Read" booksList={this.state.read}/>
         </div>
     )
   }
