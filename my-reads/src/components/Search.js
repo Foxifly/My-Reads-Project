49c3fde3bc78ae1ac64e9b9fc4bb0a39dae417a0
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-09-14 06:37:05 -0700

    Feat: Search bar that actually searches books
    TODO: Fix the space issue.

--- my-reads/src/components/Search.js
+++ my-reads/src/components/Search.js
@@ -1,27 +1,80 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import Book from "./Book";
 
 class Search extends Component {
+  state = {
+    query: "",
+    results: []
+  };
+
+  handleChange = query => {
+    if (query) {
+      query = query.trim()
+      BooksAPI.search(query).then(results => {
+        if (results) {
+          if (results.error === "empty query") {
+            this.setState({ results: "No Results Found" });
+          } else {
+            this.setState({ results });
+          }
+        }
+      });
+    }
+  };
   render() {
-    return(
+    const { results } = this.state;
+    return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link className="close-search" to="/">Close</Link>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            </div>
-            <input type="text" placeholder="Search by title or author"/>
-
-          </div>
+          <Link className="close-search" to="/">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper" />
+          <input
+            onChange={event => {
+              this.handleChange(event.target.value);
+            }}
+            type="text"
+            placeholder="Search by title or author"
+          />
         </div>
-    )
+
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {results === "No Results Found" && <h1>No results found!</h1>}
+            {results !== "No Results Found" &&
+              results.map(book => {
+                if (book.id && book.title && book.authors && book.imageLinks) {
+                  return (
+                    <Book
+                      key={book.id}
+                      id={book.id}
+                      title={book.title}
+                      author={book.authors}
+                      image={book.imageLinks.thumbnail}
+                    />
+                  );
+                }
+              })}
+          </ol>
+        </div>
+      </div>
+    );
   }
 }
 
 export default Search;
+
+/*
+/*    sortedQuery.map((book) => {
+    return (
+      <Book
+        key={book.id}
+        id={book.id}
+        title={book.title}
+        author={book.authors}
+        image={book.imageLinks.thumbnail}
+      />
+    );*/
